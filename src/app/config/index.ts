e8bd3573772f364193844c867ccd1d85bfd96c4e
abusaleh233 <abusaleh233@gmail.com>
2026-08-17 23:28:06 +0600

    bkash user name

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -34,7 +34,12 @@ export default {
 	cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
   	cloudinary_api_key: process.env.CLOUDINARY_API_KEY!,
 	cloudinary_api_secret : process.env.CLOUDINARY_API_SECRET!,
+	bkash_user_name : process.env.BKASH_USER_NAME!,
 	
+
+	
+
+
 
 
 
