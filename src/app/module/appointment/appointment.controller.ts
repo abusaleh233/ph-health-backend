7aa89e777570b7d5c92e330a7c7c674fcc60cf2c
abusaleh233 <abusaleh233@gmail.com>
2026-08-20 23:22:09 +0600

    appointment controller update

--- src/app/module/appointment/appointment.controller.ts
+++ src/app/module/appointment/appointment.controller.ts
@@ -1,34 +1,66 @@
-import { Request, Response } from "express";
+import type { Request, Response } from "express";
+import httpStatus from "http-status";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
-import httpStatus from "http-status"
-import { AppoinmentServices } from "./appointment.service";
+import { AppointmentServices } from "./appointment.service";
 
 const bookAppointment = catchAsync(async (req: Request, res: Response) => {
-	const result = await AppoinmentServices.bookAppointment()
+	const payload = req.body;
+	const user = req.user!;
 
+	const result = await AppointmentServices.bookAppointment(payload, user);
 	sendResponse(res, {
 		statusCode: httpStatus.OK,
 		success: true,
-		message: "User profile fetched successfully",
+		message: "Appointment Payment Initiated Successfully",
 		data: result,
 	});
 });
 
-const bookAppointmentcallback = catchAsync(async (req: Request, res: Response) => {
-	const {executedPaymentResult, redirectUrl} = await  AppoinmentServices.bookAppointmentCallback(req.query)
+const payAppointment = catchAsync(async (req: Request, res: Response) => {
+	const payload = req.body;
+	const user = req.user!;
 
-    console.log({executedPaymentResult}, "callback controller")
-    res.redirect(redirectUrl);
-	// sendResponse(res, {
-	// 	statusCode: httpStatus.OK,
-	// 	success: true,
-	// 	message: "User profile fetched successfully",
-	// 	data: result,
-	// });
+	const result = await AppointmentServices.payAppointment(payload, user);
+	sendResponse(res, {
+		statusCode: httpStatus.OK,
+		success: true,
+		message: "Appointment Payment Initiated Successfully",
+		data: result,
+	});
 });
 
-export const AppointmentController ={
-    bookAppointment,
-    bookAppointmentcallback 
-}
+const bookAppointmentCallback = catchAsync(
+	async (req: Request, res: Response) => {
+		const { redirectUrl } = await AppointmentServices.bookAppointmentCallback(
+			req.query,
+		);
+
+		res.redirect(redirectUrl);
+		// sendResponse(res, {
+		//     statusCode: httpStatus.OK,
+		//     success: true,
+		//     message: "User profile fetched successfully",
+		//     data: result,
+		// });
+	},
+);
+
+const cancelAppointment = catchAsync(async (req: Request, res: Response) => {
+	const payload = req.body;
+
+	const result = await AppointmentServices.cancelAppointment(payload);
+	sendResponse(res, {
+		statusCode: httpStatus.OK,
+		success: true,
+		message: "Appointment Cancelled And Refunded Successfully",
+		data: result,
+	});
+});
+
+export const AppointmentController = {
+	bookAppointment,
+	payAppointment,
+	bookAppointmentCallback,
+	cancelAppointment,
+};
