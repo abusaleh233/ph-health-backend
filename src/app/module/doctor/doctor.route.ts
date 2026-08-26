7a677028d2fefc32211aee76c5206d73b0e647af
abusaleh233 <abusaleh233@gmail.com>
2026-08-26 21:32:51 +0600

    doctor route

--- src/app/module/doctor/doctor.route.ts
+++ src/app/module/doctor/doctor.route.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import { Role } from "../../../generated/prisma/enums";
 import { upload } from "../../lib/multer";
+import { auth } from "../../middleware/checkAuth";
 import { DoctorController } from "./doctor.controller";
 
 const router = Router();
 
 router.post(
 	"/apply-as-doctor",
+	// validateRequest(UserValidation.ResetPasswordZodSchema),
 	upload.fields([
 		{
 			name: "resume",
@@ -18,5 +21,19 @@ router.post(
 		},
 	]),
 	DoctorController.applyAsDoctor,
+);
+router.post(
+	"/apply-as-doctor/verify-email",
+	DoctorController.verifyDoctorEmail,
+);
+router.post(
+	"/approve-doctor",
+	auth(Role.ADMIN, Role.SUPER_ADMIN),
+	DoctorController.verifyDoctorEmail,
+);
+router.get(
+	"/all-doctors",
+	auth(Role.ADMIN, Role.SUPER_ADMIN),
+	DoctorController.getAllDoctors,
 );
 export const DoctorRoutes = router;
